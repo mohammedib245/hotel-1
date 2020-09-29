4b760a5bd4ada48dbce9947b7c7e23e3d2e1ff78
kubila <dolaskyasin@gmail.com>
2020-09-29 23:33:12 +0300

    RoomTypeFactory

--- tests/Feature/ShowRoomsControllerTest.php
+++ tests/Feature/ShowRoomsControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+//use Illuminate\Foundation\Testing\RefreshDatabase;
+//use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ShowRoomsControllerTest extends TestCase
@@ -13,8 +15,11 @@ class ShowRoomsControllerTest extends TestCase
      */
     public function testExample()
     {
-        $response = $this->get('/test');
+        $response = $this->get('/rooms');
 
-        $response->assertStatus(200);
+        $response->assertStatus(200)
+            ->assertSeeText('Type')
+            ->assertViewIs('rooms.index')
+            ->assertViewHas('rooms');
     }
 }
